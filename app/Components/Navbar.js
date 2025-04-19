be4e8c8de773aa076052f9d920c8740c313a8c3e
Rafat Parween <109865229+rafatparween@users.noreply.github.com>
2025-04-19 22:55:39 +0530

    Update Navbar.js

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
           <div className="flex items-center">
             <Link legacyBehavior href="/">
               <a className="text-2xl font-bold text-blue-600">
-              Ij Printer Live Solution
+              Micro Hub Care
               </a>
             </Link>
           </div>
